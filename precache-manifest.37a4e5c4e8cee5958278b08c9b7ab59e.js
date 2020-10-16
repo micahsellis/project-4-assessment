9f5ae3f57f6c62729bba2bf04f87a27396192b11
Micah Ellis <micahsellis@gmail.com>
2020-10-16 14:06:17 -0600

    Updates

---
+++ precache-manifest.37a4e5c4e8cee5958278b08c9b7ab59e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db9fbde989cb2957122941f260cb5d1",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "c31a107a8f7917ebd9ea",
+    "url": "/project-4-assessment/static/css/main.e21ef515.chunk.css"
+  },
+  {
+    "revision": "4322b794b9316550d8ea",
+    "url": "/project-4-assessment/static/js/2.3976766f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.3976766f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c31a107a8f7917ebd9ea",
+    "url": "/project-4-assessment/static/js/main.5f36557e.chunk.js"
+  },
+  {
+    "revision": "25bc9098bfe694cac21f",
+    "url": "/project-4-assessment/static/js/runtime-main.cd4067b7.js"
+  }
+]);
